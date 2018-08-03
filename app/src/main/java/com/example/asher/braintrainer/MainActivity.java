d7b3fd6d6c5cbdc0aeb6809f05d12bb75e132580
Asherx15 <asher.faisal@gmail.com>
2018-08-02 20:33:46 -0400

    updated timer, score and playAgain feature

--- app/src/main/java/com/example/asher/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/asher/braintrainer/MainActivity.java
@@ -14,62 +14,56 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    CountDownTimer timer = new CountDownTimer(30000, 1000){
-        public void onTick(long m){
-            TextView timerView =(TextView)findViewById(R.id.timerView);
-            timerView.setText(String.valueOf(m/1000)+ "s");
-        }
-        public void onFinish(){
-
-        }
-    };
-
-    Button goButton;
-    TextView result;
-    TextView scoreView;
-
+    TextView result, scoreView, question;
+    Button button1, button2, button3, button4, play;
     ArrayList<Integer> answers = new ArrayList<Integer>();
     int locateAns, score, numOfQuestions;
 
+    public void playAgain(){
+        score =0;
+        numOfQuestions=0;
+
+        CountDownTimer timer = new CountDownTimer(30000, 1000){
+            public void onTick(long m){
+                TextView timerView =(TextView)findViewById(R.id.timerView);
+                timerView.setText(String.valueOf(m/1000)+ "s");
+            }
+            public void onFinish(){
+                play.setVisibility(View.VISIBLE);
+                result.setText("Times UP!");
+            }
+        };
+
+        timer.start();
+
+
+    }
 
     public void chooseAns(View view){
         if(Integer.toString(locateAns).equals(view.getTag().toString())){
             result.setText("Correct!");
-        }else
-            result.setText("Wrong");
-
+            nextQuestion();
+            score++;
+        }else {
+            result.setText("Incorrect");
+            nextQuestion();
+        }
         numOfQuestions++;
         scoreView.setText(Integer.toString(score)+ "/" + Integer.toString(numOfQuestions));
     }
 
-
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView question = findViewById(R.id.questionView);
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-
-
+    public void nextQuestion (){
         Random rand = new Random();
 
         // Correct sum
         int a = rand.nextInt(21);
         int b = rand.nextInt(21);
-        int sum = a + b;
 
 
         question.setText(Integer.toString(a)+ " + " + Integer.toString(b));
 
         locateAns = rand.nextInt(4);
+        answers.clear();
 
         for (int i =0; i<4; i++){
             if (i==locateAns){
@@ -88,8 +82,30 @@ public class MainActivity extends AppCompatActivity {
         button2.setText(Integer.toString(answers.get(1)));
         button3.setText(Integer.toString(answers.get(2)));
         button4.setText(Integer.toString(answers.get(3)));
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        question = findViewById(R.id.questionView);
         result = findViewById(R.id.resultView);
         scoreView = findViewById(R.id.pointsView);
+        play = findViewById(R.id.play);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+
+        nextQuestion();
+
+        playAgain();
+
 
     }
 }
